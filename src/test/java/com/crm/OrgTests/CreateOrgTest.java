84d3c81f8ce9200e8ad2412fa9ab94625c8e1e5e
Chaitra9632 <chaitra.sreenivas88@gmail.com>
2022-03-24 11:26:32 +0530

    User 1 first commit

---
+++ src/test/java/com/crm/OrgTests/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package com.crm.OrgTests;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("User 1 created organization");
+	}
+
+}
